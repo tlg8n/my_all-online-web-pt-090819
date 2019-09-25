e9d604edda7581f43c198ab8e9b6f1595d337b00
John Schmidley <tlg8n@outlook.com>
2019-09-25 20:15:25 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,8 +4,8 @@ def my_all?(collection)
   i = 0
   block_return_values = []
   while i < collection.length
+  
     yield(collection[i])
     i = i + 1
-    block_return_values << yield(collection[i])
   end
 end
